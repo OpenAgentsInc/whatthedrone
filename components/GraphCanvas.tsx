2b23304fd9fa91099c607d766a5e35c9e5e693e8
Christopher David <chris@openagents.com>
2024-12-16 15:37:37 -0600

    Refactor GraphCanvas to improve rendering performance and code readability.

--- components/GraphCanvas.tsx
+++ components/GraphCanvas.tsx
@@ -322,5 +322,6 @@ const styles = StyleSheet.create({
     textShadowColor: 'rgba(0, 0, 0, 0.75)',
     textShadowOffset: { width: 1, height: 1 },
     textShadowRadius: 2,
+    fontFamily: 'JetBrainsMono',
   },
 });
